9bb156925f4468e256cab98913c7b5b4cb16fd6c
Steven Harland <stvnhrlnd@gmail.com>
2018-06-29 23:10:01 +0100

    Add sign in and sign out forms

---
+++ src/Umbraco.SampleSite/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using Umbraco.SampleSite.Models;
+using Umbraco.Web.Mvc;
+
+namespace Umbraco.SampleSite.Controllers
+{
+    public class AccountController : SurfaceController
+    {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignIn(SignInModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            if (!Members.Login(model.Username, model.Password))
+            {
+                ModelState.AddModelError("", "Invalid login attempt.");
+                return CurrentUmbracoPage();
+            }
+
+            return Redirect("/");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignOut()
+        {
+            Members.Logout();
+            return Redirect("/");
+        }
+    }
+}
