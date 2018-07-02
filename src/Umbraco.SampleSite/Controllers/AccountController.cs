8693ea9d15c27c6a54aa9bc6059a2df0f4f7e812
Steven Harland <stvnhrlnd@gmail.com>
2018-07-02 22:12:37 +0100

    Add register page

--- src/Umbraco.SampleSite/Controllers/AccountController.cs
+++ src/Umbraco.SampleSite/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Security;
 using Umbraco.SampleSite.Models;
 using Umbraco.Web.Mvc;
 
@@ -30,6 +32,63 @@ namespace Umbraco.SampleSite.Controllers
         {
             Members.Logout();
             return Redirect("/");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            var registrationModel = Members.CreateRegistrationModel();
+            registrationModel.Name = $"{model.FirstName} {model.Surname}";
+            registrationModel.Email = model.Email;
+            registrationModel.Username = model.Username;
+            registrationModel.UsernameIsEmail = false;
+            registrationModel.Password = model.Password;
+            registrationModel.LoginOnSuccess = true;
+
+            var member = Members.RegisterMember(
+                registrationModel,
+                out MembershipCreateStatus status,
+                registrationModel.LoginOnSuccess);
+
+            switch (status)
+            {
+                case MembershipCreateStatus.Success:
+                    return Redirect("/");
+                case MembershipCreateStatus.InvalidUserName:
+                    ModelState.AddModelError("Username", "Invalid username.");
+                    break;
+                case MembershipCreateStatus.InvalidPassword:
+                    ModelState.AddModelError("Password", "Invalid password.");
+                    break;
+                case MembershipCreateStatus.InvalidQuestion:
+                case MembershipCreateStatus.InvalidAnswer:
+                    throw new NotImplementedException(status.ToString());
+                case MembershipCreateStatus.InvalidEmail:
+                    ModelState.AddModelError("Username", "Invalid email.");
+                    break;
+                case MembershipCreateStatus.DuplicateUserName:
+                    ModelState.AddModelError("Username", "A member with this username already exists.");
+                    break;
+                case MembershipCreateStatus.DuplicateEmail:
+                    ModelState.AddModelError("Email", "A member with this email already exists.");
+                    break;
+                case MembershipCreateStatus.UserRejected:
+                case MembershipCreateStatus.InvalidProviderUserKey:
+                case MembershipCreateStatus.DuplicateProviderUserKey:
+                case MembershipCreateStatus.ProviderError:
+                    ModelState.AddModelError("", $"An error occurred: {status}");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            return CurrentUmbracoPage();
         }
     }
 }
